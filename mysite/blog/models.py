21adb2e94cd14494d3c5971a7728a575528f3067
Henry X1 <henryqbonilla@gmail.com>
2024-05-19 23:12:32 -0400

    added a couple views

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -2,6 +2,10 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return (super().get_queryset().filter(status=Post.Status.PUBLISHED))
+
 # first model for Posts
 class Post(models.Model):
     class Status(models.TextChoices):
@@ -16,6 +20,9 @@ class Post(models.Model):
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=2, choices=Status, default=Status.DRAFT)
     
+    objects = models.Manager()
+    published = PublishedManager()
+
     class Meta:
         ordering = ['-publish']
         indexes = [
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+from django.http import Http404
 
 # Create your views here.
+
+#return a list of all published posts using custom models manager, published
+def post_list(request):
+    posts = Post.published.all()
+    return render(request, 'blog/post/list.html', {'posts': posts})
+
+#returns a single post if it exist otherwise return error 404
+def post_detail(request, id):
+    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+    return render(request, 'blog/post/detail.html', {'post': post})
